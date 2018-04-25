a9b8e945d02f249937d68c4588a515facaa9d852
yangjian <809127232@qq.com>
2018-04-25 22:21:59 +0800

    User模型添加邮箱验证码和首页轮播图模型

--- users/models.py
+++ users/models.py
@@ -1,8 +1,10 @@
+from datetime import datetime
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
 
+# Create your models here.
+#用户信息模型
 class UserProfile(AbstractUser):
     #自定义的性别选择
     GENDER_CHOICES = (('male','男'),('female','女'))
@@ -24,3 +26,30 @@ class UserProfile(AbstractUser):
 
     def __str__(self):
         return self.username
+
+#邮箱验证码模型
+class EmailVerifyRecord(models.Model):
+    SEND_CHOICES = (('register','注册'),('forget','找回密码'))
+    code = models.CharField(max_length=20,verbose_name='验证码')
+    #未设置null=True,blank=True ,默认不可为空
+    email = models.EmailField(max_length=50,verbose_name='邮箱')
+    send_type = models.CharField(choices=SEND_CHOICES,max_length=10,verbose_name='邮件类型')
+    #now去掉了（），不然会根据编译时间，而不是根据实例化时间
+    send_time = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        verbose_name = '邮箱验证码'
+        verbose_name_plural = verbose_name
+
+#轮播模型
+class Banner(models.Model):
+    title = models.CharField(max_length=100,verbose_name='标题')
+    image = models.ImageField(max_length=100,upload_to='banner/%Y/%m',verbose_name='轮播图')
+    url = models.URLField(max_length=200,verbose_name='访问地址')
+    #轮播图的显示顺序，默认很靠后
+    index = models.IntegerField(default=100,verbose_name='顺序')
+    add_time = models.DateTimeField(default=datetime.now,verbose_name='添加时间')
+
+    class Meta:
+        verbose_name = '轮播图'
+        verbose_name_plural = verbose_name
