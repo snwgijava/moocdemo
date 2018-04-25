b653bf061519588f976b38ae1fd96648877f6297
yangjian <809127232@qq.com>
2018-04-25 22:50:56 +0800

    完成所有模型设计

---
+++ courses/models.py
@@ -0,0 +1,65 @@
+from datetime import datetime
+from django.db import models
+
+# Create your models here.
+#课程信息表
+class Course(models.Model):
+    DEGREE_CHOICES = (('cj','初级'),('zj','中级'),('gj','高级'))
+    name = models.CharField(max_length=50,verbose_name='课程名')
+    desc = models.CharField(max_length=300,verbose_name='课程描述')
+    detail = models.TextField(verbose_name='课程详情')
+    degree = models.CharField(choices=DEGREE_CHOICES,max_length=2)
+    learn_times = models.IntegerField(default=0,verbose_name='学习时长(分钟数)')
+    #保存学习人数，点击开始学习才算
+    students = models.IntegerField(default=0,verbose_name='学习人数')
+    fav_nums = models.IntegerField(default=0,verbose_name='收藏人数')
+    iamge = models.ImageField(upload_to='courses/%Y/%m',verbose_name='封面图',max_length=100)
+    #保存点击量,点进页面就算
+    click_nums = models.IntegerField(default=0,verbose_name='点击数')
+    add_time = models.DateTimeField(default=datetime.now,verbose_name='添加时间')
+
+    class Meta:
+        verbose_name = '课程信息表'
+        verbose_name_plural = verbose_name
+
+#章节
+class Lesson(models.Model):
+    #一个课程对应多个章节
+    course = models.ForeignKey(Course,verbose_name='课程')
+    name = models.CharField(max_length=100,verbose_name='章节名')
+    add_time = models.DateTimeField(default=datetime.now,verbose_name='添加时间')
+
+    class Meta:
+        verbose_name = '章节'
+        verbose_name_plural = verbose_name
+
+# 每章视频
+class Video(models.Model):
+    # 因为一个章节对应很多视频。所以在视频表中将章节设置为外键。
+    # 作为一个字段来存储让我们可以知道这个视频对应哪个章节.
+    lesson = models.ForeignKey(Lesson, verbose_name=u"章节")
+    name = models.CharField(max_length=100, verbose_name=u"视频名")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
+
+    class Meta:
+        verbose_name = u"视频"
+        verbose_name_plural = verbose_name
+
+
+# 课程资源
+class CourseResource(models.Model):
+    # 因为一个课程对应很多资源。所以在课程资源表中将课程设置为外键。
+    # 作为一个字段来让我们可以知道这个资源对应那个课程
+    course = models.ForeignKey(Course, verbose_name=u"课程")
+    name = models.CharField(max_length=100, verbose_name=u"名称")
+    # 这里定义成文件类型的field，后台管理系统中会直接有上传的按钮。
+    # FileField也是一个字符串类型，要指定最大长度。
+    download = models.FileField(
+        upload_to="course/resource/%Y/%m",
+        verbose_name=u"资源文件",
+        max_length=100)
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
+
+    class Meta:
+        verbose_name = u"课程资源"
+        verbose_name_plural = verbose_name
